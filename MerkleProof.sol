825a88ffa599eb7119ded61f2412312f8c87dc6d
berkingurcan <berkingurcan@yandex.com>
2022-05-11 10:37:04 +0300

    Update MerkleProof.sol

--- MerkleProof.sol
+++ MerkleProof.sol
@@ -4,7 +4,7 @@ contract MerkleProof {
     function verify(bytes32[] memory proof, bytes32 root, bytes32 leaf, uint index) public pure returns(bool) {
         bytes32 hash = leaf;
         for(uint i = 0; i < proof.length; i++) {
-            if (i % 2 == 0) {
+            if (index % 2 == 0) {
                 hash = keccak256(abi.encodePacked(hash, proof[i]));
             } else {
                 hash = keccak256(abi.encodePacked(proof[i], hash));
