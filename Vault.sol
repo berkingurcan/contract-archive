4638f605d2846c4ab758051e38d800d4fe3c11d2
berkingurcan <berkingurcan@yandex.com>
2022-05-27 13:40:46 +0300

    Create Vault.sol

---
+++ Vault.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Idetifier: MIT
+pragma solidity ^0.8.13;
+
+import "./IERC20.sol";
+
+contract Vault {
+    IERC20 public immutable token;
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    function _mint(address _to, uint _amount) private {
+        totalSupply += _amount;
+        balanceOf[_to] += _amount;
+    }
+
+    function _burn(address _from, uint _amount) private {
+        totalSupply -= _amount;
+        balanceOf[_] -= _amount;
+    }
+
+    function deposit(uint _amount) external {
+        uint shares;
+        if (totalSupply == 0) {
+            shares = amount;
+        } else {
+            shares = (_amount * totalSupply) / token.balanceOf(address(this));
+        }
+
+        _mint(msg.sender, shares);
+        token.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    function withdraw(uint _shares) external {
+        uint amount = (_shares * token.balanceOf(address(this))) / totalSupply; 
+        _burn(msg.sender, _shares);
+        token.transfer(msg.sender, amount);
+    }
+}
